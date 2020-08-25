2d91d7335ffd61b43bb3c9e106f28ba2d8fc21c7
Anthony James <tjames84@gmail.com>
2020-08-25 08:04:46 -0600

    Rendering our Filtered Data to the screen

--- JavaScript in the Browser/j-00-COMPLETED APP/notes-app/notes-app.js
+++ JavaScript in the Browser/j-00-COMPLETED APP/notes-app/notes-app.js
@@ -1,24 +1,49 @@
-const notes = [{
-    title: 'my next trip',
-    body: 'I would like to go to Spain'
-}, {
-    title: 'Habbits to work on',
-    body: 'Exercise. Eating a bit better.'
-}, {
-    title: 'Office modification',
-    body: 'Get a new seat'
-}]
+const notes = [
+  {
+    title: "my next trip",
+    body: "I would like to go to Spain",
+  },
+  {
+    title: "Habbits to work on",
+    body: "Exercise. Eating a bit better.",
+  },
+  {
+    title: "Office modification",
+    body: "Get a new seat",
+  },
+];
 
-document.querySelector('#create-note').addEventListener('click', function (e) {
-    e.target.textContent = 'The button was clicked'
-})
+const filters = {
+  searchText: "",
+};
 
-document.querySelector('#remove-all').addEventListener('click', function () {
-    document.querySelectorAll('.note').forEach(function( note) {
-        note.remove()
-    })
-})
+const renderNotes = (notes, filters) => {
+  const filteredNotes = notes.filter((note) => {
+    return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
 
-document.querySelector('#search-text').addEventListener('input', e => {
-    console.log(e.target.value)
-})
+  document.querySelector("#notes").innerHTML = "";
+
+  filteredNotes.forEach((note) => {
+    const noteEl = document.createElement("p");
+    noteEl.textContent = note.title;
+    document.querySelector("#notes").appendChild(noteEl);
+  });
+};
+
+renderNotes(notes, filters);
+
+document.querySelector("#create-note").addEventListener("click", function (e) {
+  e.target.textContent = "The button was clicked";
+});
+
+document.querySelector("#remove-all").addEventListener("click", function () {
+  document.querySelectorAll(".note").forEach(function (note) {
+    note.remove();
+  });
+});
+
+document.querySelector("#search-text").addEventListener("input", (e) => {
+  filters.searchText = e.target.value;
+  renderNotes(notes, filters);
+});
