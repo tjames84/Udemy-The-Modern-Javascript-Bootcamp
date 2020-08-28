04739ae969f1d97618f9652660033de78f46e626
Anthony James <tjames84@gmail.com>
2020-08-28 16:36:23 -0600

    Setting up the Prototype Object

--- 00 COMPLETE APP CHALLENGES/hangman/hangman.js
+++ 00 COMPLETE APP CHALLENGES/hangman/hangman.js
@@ -1,10 +1,9 @@
 const Hangman = function (word, remainingGuesses) {
-    this.word = word,
-    this.remainingGuesses = remainingGuesses
-}
+  (this.word = word), (this.remainingGuesses = remainingGuesses);
+};
 
-const Game1 = new Hangman('Cat', 2)
-console.log(Game1)
+const Game1 = new Hangman("Cat", 2);
+console.log(Game1);
 
-const Game2 = new Hangman('Colorado', 4)
-console.log(Game2)
+const Game2 = new Hangman("Colorado", 4);
+console.log(Game2);
