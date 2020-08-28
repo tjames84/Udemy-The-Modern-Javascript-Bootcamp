0b96dc041c51d6d67093d89d687e32971a4c886e
Anthony James <tjames84@gmail.com>
2020-08-28 16:47:05 -0600

    Hangman Challenge Part 1

--- 00 COMPLETE APP CHALLENGES/hangman/hangman.js
+++ 00 COMPLETE APP CHALLENGES/hangman/hangman.js
@@ -1,9 +1,25 @@
 const Hangman = function (word, remainingGuesses) {
-  (this.word = word), (this.remainingGuesses = remainingGuesses);
+  (this.word = word.toLowerCase().split("")),
+    (this.remainingGuesses = remainingGuesses),
+    (this.guessedLetters = ["c", "e"]);
+};
+
+Hangman.prototype.getPuzzle = function () {
+  let puzzle = "";
+
+  this.word.forEach((letter) => {
+    if (this.guessedLetters.includes(letter) || letter === " ") {
+      puzzle += letter;
+    } else {
+      puzzle += "*";
+    }
+  });
+
+  return puzzle;
 };
 
 const Game1 = new Hangman("Cat", 2);
-console.log(Game1);
+console.log(Game1.getPuzzle());
 
-const Game2 = new Hangman("Colorado", 4);
-console.log(Game2);
+const Game2 = new Hangman("Colorado denver", 4);
+console.log(Game2.getPuzzle());
