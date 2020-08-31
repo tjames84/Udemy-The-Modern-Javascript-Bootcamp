fed46a5cf745c006723e724e1f996d7ccfcbbf39
Anthony James <tjames84@gmail.com>
2020-08-31 13:11:18 -0600

    HTTP headers and errors

--- 00 COMPLETE APP CHALLENGES/hangman/app.js
+++ 00 COMPLETE APP CHALLENGES/hangman/app.js
@@ -12,24 +12,26 @@ const game1 = new Hangman(randWord, totalGuesses);
 puzzleEl.textContent = game1.puzzle;
 guessesEl.textContent = game1.statusMessage;
 
-
-window.addEventListener("keypress", function (e) {
+window.addEventListener("keypress", (e) => {
   const guess = String.fromCharCode(e.charCode);
   game1.makeGuess(guess);
   puzzleEl.textContent = game1.puzzle;
   guessesEl.textContent = game1.statusMessage;
-
 });
 
 // Making an HTTP request
+// httpstatuses.com
+// mdn http message
 const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4) {
+request.addEventListener("readystatechange", (e) => {
+  if (e.target.readyState === 4 && e.target.status === 200) {
     const data = JSON.parse(e.target.responseText);
-    console.log(data)
+    console.log(data);
+  } else if (e.target.readyState === 4) {
+    console.log("An error has taken place");
   }
-})
+});
 
-request.open('GET', 'http://puzzle.mead.io/puzzle');
+request.open("GET", "http://puzzle.mead.io/puzzle");
 request.send();
