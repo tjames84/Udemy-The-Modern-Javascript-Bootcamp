143829818443f3d4c4fd14dc96c0254eac4e90b1
Anthony James <tjames84@gmail.com>
2020-08-31 13:26:12 -0600

    Exploring anther API. Country Finder app in COMPLETE APP CHALLENGES created.

---
+++ 00 COMPLETE APP CHALLENGES/country-finder/app.js
@@ -0,0 +1,20 @@
+// Making an HTTP request
+// httpstatuses.com
+// mdn http message
+// https://restcountries.eu/rest/v2/all
+
+const countryCode = "US";
+const countryRequest = new XMLHttpRequest();
+
+countryRequest.addEventListener("readystatechange", (e) => {
+  if (e.target.readyState === 4 && e.target.status === 200) {
+    const data = JSON.parse(e.target.responseText);
+    const country = data.find((country) => country.alpha2Code === countryCode);
+    console.log(country.name);
+  } else if (e.target.readyState === 4) {
+    console.log("Unable to fetch data");
+  }
+});
+
+countryRequest.open("GET", "https://restcountries.eu/rest/v2/all");
+countryRequest.send();
