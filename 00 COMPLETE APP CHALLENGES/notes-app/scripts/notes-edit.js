62f9afb24db419d3e18bb59b5d9c0589180e2861
Anthony James <tjames84@gmail.com>
2020-09-01 12:43:59 -0600

    notes app COMPLETE

--- 00 COMPLETE APP CHALLENGES/notes-app/scripts/notes-edit.js
+++ 00 COMPLETE APP CHALLENGES/notes-app/scripts/notes-edit.js
@@ -5,12 +5,9 @@ const titleElement = document.querySelector("#note-title");
 const bodyElement = document.querySelector("#note-body");
 const removeElement = document.querySelector("#remove-note");
 const dateElement = document.querySelector("#last-edited");
-
 const noteId = location.hash.substring(1);
 let notes = getSavedNotes();
-let note = notes.find((note) => {
-  return note.id === noteId;
-});
+let note = notes.find((note) => note.id === noteId);
 
 if (!note) {
   location.assign("/index.html");
@@ -18,38 +15,32 @@ if (!note) {
 
 titleElement.value = note.title;
 bodyElement.value = note.body;
-
-dateElement.textContent = generateLastEditied(note.updatedAt);
+dateElement.textContent = generateLastEdited(note.updatedAt);
 
 titleElement.addEventListener("input", (e) => {
   note.title = e.target.value;
   note.updatedAt = moment().valueOf();
-  dateElement.textContent = generateLastEditied(note.updatedAt);
-
+  dateElement.textContent = generateLastEdited(note.updatedAt);
   saveNotes(notes);
 });
 
 bodyElement.addEventListener("input", (e) => {
   note.body = e.target.value;
   note.updatedAt = moment().valueOf();
-  dateElement.textContent = generateLastEditied(note.updatedAt);
-
+  dateElement.textContent = generateLastEdited(note.updatedAt);
   saveNotes(notes);
 });
 
 removeElement.addEventListener("click", (e) => {
   removeNote(note.id);
   saveNotes(notes);
-
   location.assign("/index.html");
 });
 
 window.addEventListener("storage", (e) => {
   if (e.key === "notes") {
     notes = JSON.parse(e.newValue);
-    note = notes.find((note) => {
-      return note.id === noteId;
-    });
+    note = notes.find((note) => note.id === noteId);
 
     if (!note) {
       location.assign("/index.html");
@@ -57,7 +48,6 @@ window.addEventListener("storage", (e) => {
 
     titleElement.value = note.title;
     bodyElement.value = note.body;
-    dateElement.textContent = generateLastEditied(note.updatedAt);
-
+    dateElement.textContent = generateLastEdited(note.updatedAt);
   }
 });
