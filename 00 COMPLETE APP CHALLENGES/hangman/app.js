7199c501ab5065c1cd1e131330febdcbf668e158
Anthony James <tjames84@gmail.com>
2020-09-01 10:14:15 -0600

    integrating Data into our apps. changed hangman app to have a reset button

--- 00 COMPLETE APP CHALLENGES/hangman/app.js
+++ 00 COMPLETE APP CHALLENGES/hangman/app.js
@@ -1,25 +1,36 @@
 const puzzleEl = document.querySelector("#puzzle");
 const guessesEl = document.querySelector("#guesses");
 
-const game1 = new Hangman("Car Parts", 2);
-
-puzzleEl.textContent = game1.puzzle;
-guessesEl.textContent = game1.statusMessage;
+let game1;
 
 window.addEventListener("keypress", (e) => {
   const guess = String.fromCharCode(e.charCode);
   game1.makeGuess(guess);
-  puzzleEl.textContent = game1.puzzle;
-  guessesEl.textContent = game1.statusMessage;
+  render();
 });
 
-getPuzzle("2")
-  .then((puzzle) => {
-    console.log(puzzle);
-  })
-  .catch((err) => {
-    console.log(`Error: ${err}`);
-  });
+const render = () => {
+  puzzleEl.textContent = game1.puzzle;
+  guessesEl.textContent = game1.statusMessage;
+};
+
+const startGame = async () => {
+  const puzzle = await getPuzzle("2");
+  game1 = new Hangman(puzzle, 5);
+  render();
+};
+
+document.querySelector("#reset").addEventListener("click", startGame);
+
+startGame();
+
+// getPuzzle("2")
+//   .then((puzzle) => {
+//     console.log(puzzle);
+//   })
+//   .catch((err) => {
+//     console.log(`Error: ${err}`);
+//   });
 
 // OLD *************************************************************
 // Asynchornous
