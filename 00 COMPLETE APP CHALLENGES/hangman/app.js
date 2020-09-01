08e653f4c986a707f3cf873e093ca665bd6504d5
Anthony James <tjames84@gmail.com>
2020-08-31 19:51:07 -0600

    Fetch API updated the hangman app to use fetch and promises

--- 00 COMPLETE APP CHALLENGES/hangman/app.js
+++ 00 COMPLETE APP CHALLENGES/hangman/app.js
@@ -13,14 +13,13 @@ window.addEventListener("keypress", (e) => {
   guessesEl.textContent = game1.statusMessage;
 });
 
-getPuzzle("2").then(
-  (puzzle) => {
+getPuzzle("2")
+  .then((puzzle) => {
     console.log(puzzle);
-  },
-  (err) => {
+  })
+  .catch((err) => {
     console.log(`Error: ${err}`);
-  }
-);
+  });
 
 // Asynchornous
 // getPuzzle("2", (error, puzzle) => {
@@ -33,3 +32,19 @@ getPuzzle("2").then(
 
 // const puzzle = getPuzzle();
 // console.log(puzzle)
+
+// fetch( url, {} )
+// fetch("http://puzzle.mead.io/puzzle", {})
+//   .then((response) => {
+//     if (response.status === 200) {
+//       return response.json();
+//     } else {
+//       throw new Error("Unable to fetch the puzzle");
+//     }
+//   })
+//   .then((data) => {
+//     console.log(data.puzzle);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
