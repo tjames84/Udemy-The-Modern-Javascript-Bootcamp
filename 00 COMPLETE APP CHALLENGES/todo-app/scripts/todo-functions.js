77701434e0e5c1147b27be18625f142e92306f09
Anthony James <tjames84@gmail.com>
2020-09-01 12:52:31 -0600

    Todo App COMPLETE

--- 00 COMPLETE APP CHALLENGES/todo-app/scripts/todo-functions.js
+++ 00 COMPLETE APP CHALLENGES/todo-app/scripts/todo-functions.js
@@ -14,19 +14,16 @@ const getSavedTodos = () => {
   } catch (e) {
     return [];
   }
-
 };
 
-// Save todos to localStroage
+// Save todos to localStorage
 const saveTodos = (todos) => {
   localStorage.setItem("todos", JSON.stringify(todos));
 };
 
-// remove todo from list
+// Remove todo by id
 const removeTodo = (id) => {
-  const todoIndex = todos.findIndex((todo) => {
-    return todo.id === id;
-  });
+  const todoIndex = todos.findIndex((todo) => todo.id === id);
 
   if (todoIndex > -1) {
     todos.splice(todoIndex, 1);
@@ -35,9 +32,7 @@ const removeTodo = (id) => {
 
 // Toggle the completed value for a given todo
 const toggleTodo = (id) => {
-  const todo = todos.find((todo) => {
-    return todo.id === id;
-  });
+  const todo = todos.find((todo) => todo.id === id);
 
   if (todo) {
     todo.completed = !todo.completed;
@@ -46,6 +41,7 @@ const toggleTodo = (id) => {
 
 // Render application todos based on filters
 const renderTodos = (todos, filters) => {
+  const todoEl = document.querySelector("#todos");
   const filteredTodos = todos.filter((todo) => {
     const searchTextMatch = todo.text
       .toLowerCase()
@@ -54,50 +50,59 @@ const renderTodos = (todos, filters) => {
 
     return searchTextMatch && hideCompletedMatch;
   });
+  const incompleteTodos = filteredTodos.filter((todo) => !todo.completed);
 
-  const incompleteTodos = filteredTodos.filter(function (todo) {
-    return !todo.completed;
-  });
+  todoEl.innerHTML = "";
+  todoEl.appendChild(generateSummaryDOM(incompleteTodos));
 
-  document.querySelector("#todos").innerHTML = "";
-  document
-    .querySelector("#todos")
-    .appendChild(generateSummaryDOM(incompleteTodos));
-
-  filteredTodos.forEach((todo) => {
-    document.querySelector("#todos").appendChild(generateTodoDOM(todo));
-  });
+  if (filteredTodos.length > 0) {
+    filteredTodos.forEach((todo) => {
+      todoEl.appendChild(generateTodoDOM(todo));
+    });
+  } else {
+    const messageEl = document.createElement("p");
+    messageEl.classList.add("empty-message");
+    messageEl.textContent = "No to-dos to show";
+    todoEl.appendChild(messageEl);
+  }
 };
 
-// Get the DOM elements for an individual todo
+// Get the DOM elements for an individual note
 const generateTodoDOM = (todo) => {
-  const todoEl = document.createElement("div");
+  const todoEl = document.createElement("label");
+  const containerEl = document.createElement("div");
   const checkbox = document.createElement("input");
   const todoText = document.createElement("span");
   const removeButton = document.createElement("button");
 
   // Setup todo checkbox
-  checkbox.setAttribute('type', 'checkbox');
+  checkbox.setAttribute("type", "checkbox");
   checkbox.checked = todo.completed;
-  todoEl.appendChild(checkbox);
+  containerEl.appendChild(checkbox);
   checkbox.addEventListener("change", () => {
     toggleTodo(todo.id);
     saveTodos(todos);
     renderTodos(todos, filters);
-  })
+  });
 
   // Setup the todo text
   todoText.textContent = todo.text;
-  todoEl.appendChild(todoText);
+  containerEl.appendChild(todoText);
+
+  // Setup container
+  todoEl.classList.add("list-item");
+  containerEl.classList.add("list-item__container");
+  todoEl.appendChild(containerEl);
 
   // Setup the remove button
-  removeButton.textContent = 'x';
+  removeButton.textContent = "remove";
+  removeButton.classList.add("button", "button--text");
   todoEl.appendChild(removeButton);
   removeButton.addEventListener("click", () => {
     removeTodo(todo.id);
     saveTodos(todos);
     renderTodos(todos, filters);
-  })
+  });
 
   return todoEl;
 };
@@ -105,7 +110,8 @@ const generateTodoDOM = (todo) => {
 // Get the DOM elements for list summary
 const generateSummaryDOM = (incompleteTodos) => {
   const summary = document.createElement("h2");
-  summary.textContent = `You have ${incompleteTodos.length} todos left`;
-
+  const plural = incompleteTodos.length === 1 ? "" : "s";
+  summary.classList.add("list-title");
+  summary.textContent = `You have ${incompleteTodos.length} todo${plural} left`;
   return summary;
 };
