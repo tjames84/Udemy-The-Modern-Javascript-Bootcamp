c6fadbdf8735c8362303b90318493424fc0a7731
Anthony James <tjames84@gmail.com>
2020-09-04 10:34:34 -0600

    Converted Hangman app to work with multiple browsers

--- 000-COMPLETE APP CHALLENGES/babel-boilerplate/src/index.js
+++
@@ -1,8 +0,0 @@
-import otherSquare, { add, name } from './utilities';
-import otherScream from './scream';
-
-console.log('index.js');
-
-console.log(otherScream(name))
-
-console.log(otherSquare(10))
--- 000-COMPLETE APP CHALLENGES/babel-boilerplate/src/scream.js
+++
@@ -1,7 +0,0 @@
-const scream = (str) => {
-    let scream = `${str.toUpperCase()}!`
-
-    return scream;
-}
-
-export { scream as default }
